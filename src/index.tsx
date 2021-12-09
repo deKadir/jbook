0e12a3c2c422a2c99495f7f3b04e7a73ed59a467
Your Name <you@example.com>
2021-12-10 02:15:29 +0300

    transpiling-bundling

---
+++ src/index.tsx
@@ -0,0 +1,48 @@
+import * as esbuild from "esbuild-wasm";
+import {useRef,useState,useEffect } from "react";
+import ReactDom from "react-dom";
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+
+
+const App=()=>{
+    
+    const [input,setInput]=useState('');
+    const [code,setCode]=useState('');
+    const ref=useRef<any>();
+    const startService=async()=>{
+        ref.current =await esbuild.startService({
+            worker:true,
+            wasmURL:'/esbuild.wasm'
+        });
+    }
+    useEffect(() => {
+        startService();
+    }, [])
+    const onClick=async()=>{
+        if(!ref.current){
+            return;
+        }
+        const result =await ref.current.build({
+            entryPoints:['index.js'],
+            bundle:true,
+            write:false,
+            plugins:[unpkgPathPlugin()],
+            define:{
+                'process.env.NODE_ENV':'"production"',
+                global:"window"
+            }
+            
+        })
+        setCode(result.outputFiles[0].text);
+    }
+    
+    return <div>
+        <textarea value={input} onChange={e=>setInput(e.target.value)}/>
+        <div><button onClick={onClick}>Submit</button></div>
+        <pre>{code}</pre>
+    </div>
+}
+
+ReactDom.render(
+    <App/>,document.querySelector("#root")
+)
