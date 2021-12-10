c4dd8f972eae16d02d3230924edbe68e8d7198cb
Your Name <you@example.com>
2021-12-11 01:49:26 +0300

    code execution handling

--- src/index.tsx
+++ src/index.tsx
@@ -10,8 +10,8 @@ import { fetchPlugin } from "./plugins/fetch-plugin";
 const App=()=>{
         
     const [input,setInput]=useState('');
-    const [code,setCode]=useState('');
     const ref=useRef<any>();
+    const iframe=useRef<any>();
     const startService=async()=>{
         ref.current =await esbuild.startService({
             worker:true,
@@ -25,6 +25,7 @@ const App=()=>{
         if(!ref.current){
             return;
         }
+        iframe.current.srcdoc=html;
         const result =await ref.current.build({
             entryPoints:['index.js'],
             bundle:true,
@@ -36,13 +37,36 @@ const App=()=>{
             }
             
         })
-        setCode(result.outputFiles[0].text);
+        iframe.current.contentWindow.postMessage(result.outputFiles[0].text,'*');
+       
+
     }
-    
+    const html=`
+    <html lang="en">
+    <head>
+        <title>Document</title>
+    </head>
+    <body>
+      <div id="root">
+      </div>
+      <script>
+      window.addEventListener('message',(event)=>{
+          try{
+            eval(event.data);
+          }catch(err){
+            const root=document.querySelector('#root');
+            root.innerHTML='<div style="color:red"><h4>Runtime Error:</h4> '+err+'</div>'
+          }
+        
+      },false)
+      </script>
+    </body>
+    </html>
+        `
     return <div>
         <textarea value={input} onChange={e=>setInput(e.target.value)}/>
         <div><button onClick={onClick}>Submit</button></div>
-        <pre>{code}</pre>
+        <iframe title="" ref={iframe} width={400} height={400} sandbox="allow-scripts" srcDoc={html}/>
     </div>
 }
 
